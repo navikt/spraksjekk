fe39917fb9bec7ac7709435fb2e1fa943a5c2c03
EIlif Johansen <eilifjohansen@outlook.com>
2022-08-07 12:30:00 +0200

    Endret formulering på forsiden

--- src/components/theme/Purpose.tsx
+++ src/components/theme/Purpose.tsx
@@ -3,7 +3,7 @@ import { DirectionSign, ShakeHands, EyeScreened } from "@navikt/ds-icons";
 function Purpose() {
     return (
         <div className="pb-2">
-            <p>Nav Språkhjelp hjelper deg med å gjøre tekstene mer lettlest.</p>
+            <p>NAV Språkhjelp hjelper deg med å gjøre tekstene mer lettlest.</p>
             <ul className="ListRemoveStyling">
                 <li><ShakeHands title="Håndtrykk" /> NAV lagrer ikke teksten.</li>
                 <li><EyeScreened title="Personvern"/> Ikke legg inn personopplysninger.</li>
