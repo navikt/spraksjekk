890fe26784458b63faeb7af65ea93130b8f17319
EIlif Johansen <eilifjohansen@outlook.com>
2022-11-23 00:31:28 +0100

    Endret til Aksel designtema

--- src/components/theme/About/About.tsx
+++ src/components/theme/About/About.tsx
@@ -4,7 +4,7 @@ import './About.css'
 function About() {
     return (
         <>
-            <p className="a11y-lineheight">NAV Språkhjelp hjelper deg med å gjøre tekstene mer lettlest.</p>
+            <p className="a11y-lineheight">NAV Språkhjelp hjelper deg med å gjøre tekstene dine enklere å forstå.</p>
             <ul className="ListRemoveStyling">
                 <li><ShakeHands title="Håndtrykk"/> NAV lagrer ikke teksten.</li>
                 <li><EyeScreened title="Personvern"/> Ikke legg inn personopplysninger.</li>
