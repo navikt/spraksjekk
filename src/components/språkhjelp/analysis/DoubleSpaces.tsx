cf12b2a29c38c3f3c6d3571cd6ef24dbbb5ca4d4
EIlif Johansen <eilifjohansen@outlook.com>
2022-11-26 14:01:53 +0100

    Fjernet ubrukt kode

--- src/components/språkhjelp/analysis/DoubleSpaces.tsx
+++ src/components/språkhjelp/analysis/DoubleSpaces.tsx
@@ -6,32 +6,23 @@ function DoubleSpaces(props: { content: any; }) {
     value = value.replaceAll(/\d+(?: \d+)/g, "");
     value = value.toLowerCase();
 
-    // Find dublicate words
-    let dublicateWordsList: string | number | boolean | JSX.Element[] | null | undefined = [];
-    let dublicateWordsCount = 0;
+    // Finn  tilfeller der det er mer en ett mellomrom mellom ord
+    let doubleSpacesCount = 0;
     if (value.match(/ {2,}/g)) {
-        dublicateWordsCount = value.match(/ {2,}/g).length
-
-        // @ts-ignore
-     /*   dublicateWordsList = value.match(/{2,}/g).map((duplicatedword, index) =>
-           <></>/!* <li className="språkhjelp-pb-2" key={index}ßß>{duplicatedword}</li>*!/
-        );*/
+        doubleSpacesCount = value.match(/ {2,}/g).length
     }
     return (
         <>
-            {dublicateWordsCount != 0 && (
+            {doubleSpacesCount != 0 && (
                 <Accordion.Item>
                     <Accordion.Header>
-                        {dublicateWordsCount == 1 ? (<>1 tilfelle av for mange mellomrom</>) : (<>{dublicateWordsCount} tilfeller av for mange mellomrom</>)}
+                        {doubleSpacesCount == 1 ? (<>1 tilfelle av for mange mellomrom</>) : (<>{doubleSpacesCount} tilfeller av for mange mellomrom</>)}
                     </Accordion.Header>
                     <Accordion.Content className="">
                         <Heading spacing level="3" size="xsmall">
                             Bruk ett mellomrom mellom ord.
                         </Heading>
-                        Teksten inneholder {dublicateWordsCount == 1 ? (<>ett tilfelle</>) : (<>{dublicateWordsCount} tilfeller</>)} der det er mer enn ett mellomrom mellom ord.
-                        {/*<ul className="språkhjelp-list-disc språkhjelp-pt-5 språkhjelp-list-inside">
-                            {dublicateWordsList}
-                        </ul>*/}
+                        Det er {doubleSpacesCount == 1 ? (<>ett tilfelle</>) : (<>{doubleSpacesCount} tilfeller</>)} i teksten der det er mer enn ett mellomrom mellom ord.
                     </Accordion.Content>
                 </Accordion.Item>
             )}
