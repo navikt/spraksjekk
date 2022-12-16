8d6df86336b2a173636a9cc1107c76c1d05f590a
EIlif Johansen <eilifjohansen@outlook.com>
2022-12-16 21:37:38 +0100

    Endret flytende knapper

--- src/components/theme/FloatMenu/FloatMenu.tsx
+++ src/components/theme/FloatMenu/FloatMenu.tsx
@@ -17,14 +17,19 @@ function FloatMenu(props) {
                     <Button role="link" variant="secondary" onClick={(e) => {
                         e.preventDefault();
                         window.open('https://ordbokene.no/bm,nn/search?q=' + higlighetdwords, "_blank");
-                    }}><Search/> Ordbøkene.no</Button>
+                    }}>Ordbøkene</Button>
                     <Button role="link" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
                         e.preventDefault();
-                        window.open('https://www.nb.no/ngram/#1_1_2_' + higlighetdwords + '_1_1_0_1800%2C2021_2_2_2_12_2', "_blank");
-                    }}><Search/> N-gram</Button>
+                        window.open('https://api.nb.no/dhlab/nb_ngram/#ngram/query?terms=' + higlighetdwords + '&lang=all&case_sens=0&freq=rel&corpus=avis', "_blank");
+                    }}>N-gram</Button>
+                    <Button role="link" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
+                        e.preventDefault();
+                        window.open('https://data.norge.no/concepts?q=' + higlighetdwords, "_blank");
+                    }}>Begrepskatalog</Button>
                 </>
             )}
         </BubbleMenu>
+    /*https://www.nb.no/ngram/#1_1_2_' + higlighetdwords + '_1_1_0_1800%2C2021_2_2_2_12_2*/
     );
 }
 
