47e5b2fd63c168666f50e8645ae52bbdce2e525b
EIlif Johansen <eilifjohansen@outlook.com>
2022-12-26 17:09:29 +0100

    La til CSS justeringer i modul

--- src/components/theme/FloatMenu/FloatMenu.tsx
+++ src/components/theme/FloatMenu/FloatMenu.tsx
@@ -16,15 +16,15 @@ function FloatMenu(props) {
         }} editor={editor}>
             {!higlighetdwords.match(/[?]+|[!]+|[.]+|[,]+|[:]/g) ? (
                 <>
-                    <Button role="link" size="small" variant="secondary" onClick={(e) => {
+                    <Button role="link" size="small" style={{pointerEvents: "none"}} variant="secondary" onClick={(e) => {
                         e.preventDefault();
                         window.open('https://ordbokene.no/bm,nn/search?q=' + higlighetdwords.toLowerCase(), "_blank");
                     }}>Ordbøkene</Button>
-                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
+                    <Button role="link" size="small" style={{marginLeft: '-1px', pointerEvents: "none"}} variant="secondary" onClick={(e) => {
                         e.preventDefault();
                         window.open('https://api.nb.no/dhlab/nb_ngram/#ngram/query?terms=' + higlighetdwords.toLowerCase() + '&lang=all&case_sens=0&freq=rel&corpus=avis', "_blank");
                     }}>N-gram</Button>
-                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
+                    <Button role="link" size="small" style={{marginLeft: '-1px', pointerEvents: "none"}} variant="secondary" onClick={(e) => {
                         e.preventDefault();
                         window.open('https://data.norge.no/concepts?q=' + higlighetdwords.toLowerCase(), "_blank");
                     }}>Begrepskatalog</Button>
@@ -32,7 +32,7 @@ function FloatMenu(props) {
             ) : ( <Button role="link" size="small" variant="secondary" onClick={(e) => {
                 e.preventDefault();
                 window.open('https://navikt.github.io/spraksjekk/?q=' + higlighetdwords, "_blank");
-            }}>Analyser tekst <ExternalLink style={{paddingTop : "1px"}}/>
+            }}>Analyser <ExternalLink style={{paddingTop : "1px", pointerEvents: "none"}}/>
             </Button>)}
         </BubbleMenu>
     /*https://www.nb.no/ngram/#1_1_2_' + higlighetdwords + '_1_1_0_1800%2C2021_2_2_2_12_2*/
