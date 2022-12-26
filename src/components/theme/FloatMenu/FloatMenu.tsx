75f09e41eeb801f03d5782e34d7dde337a97eed3
EIlif Johansen <eilifjohansen@outlook.com>
2022-12-26 20:47:36 +0100

    Fikset lenker i modul

--- src/components/theme/FloatMenu/FloatMenu.tsx
+++ src/components/theme/FloatMenu/FloatMenu.tsx
@@ -5,8 +5,24 @@ import {BubbleMenu} from "@tiptap/react";
 function FloatMenu(props) {
     let higlighetdwords = props.higlighetdwords
     let editor = props.editor
-    higlighetdwords = higlighetdwords.replaceAll("\n", "%20%20");
-    higlighetdwords = higlighetdwords.replaceAll("%20%20%20%20", "%20%20");
+
+    const dictionaryLink = () => {
+        window.open('https://ordbokene.no/bm,nn/search?q=' + higlighetdwords.toLowerCase(), "_blank");
+    }
+
+    const ngramLink = () => {
+        window.open('https://api.nb.no/dhlab/nb_ngram/#ngram/query?terms=' + higlighetdwords.toLowerCase() + '&lang=all&case_sens=0&freq=rel&corpus=avis', "_blank");
+    }
+
+    const datanorgeLink = () => {
+        window.open('https://data.norge.no/concepts?q=' + higlighetdwords.toLowerCase(), "_blank");
+    }
+
+    const analyzeLink = () => {
+        higlighetdwords = higlighetdwords.replaceAll("\n", "%20%20");
+        higlighetdwords = higlighetdwords.replaceAll("%20%20%20%20", "%20%20");
+        window.open('https://navikt.github.io/spraksjekk/?q=' + higlighetdwords, "_blank");
+    }
     return (
         <BubbleMenu tippyOptions={{
             aria: {
@@ -16,26 +32,21 @@ function FloatMenu(props) {
         }} editor={editor}>
             {!higlighetdwords.match(/[?]+|[!]+|[.]+|[,]+|[:]/g) ? (
                 <>
-                    <Button role="link" size="small" variant="secondary" onClick={(e) => {
-                        e.preventDefault();
-                        window.open('https://ordbokene.no/bm,nn/search?q=' + higlighetdwords.toLowerCase(), "_blank");
+                    <Button role="link" size="small" variant="secondary" onClick={() => {
+                        dictionaryLink()
                     }}>Ordbøkene</Button>
-                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
-                        e.preventDefault();
-                        window.open('https://api.nb.no/dhlab/nb_ngram/#ngram/query?terms=' + higlighetdwords.toLowerCase() + '&lang=all&case_sens=0&freq=rel&corpus=avis', "_blank");
+                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={() => {
+                        ngramLink()
                     }}>N-gram</Button>
-                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={(e) => {
-                        e.preventDefault();
-                        window.open('https://data.norge.no/concepts?q=' + higlighetdwords.toLowerCase(), "_blank");
+                    <Button role="link" size="small" style={{marginLeft: '-1px'}} variant="secondary" onClick={() => {
+                        datanorgeLink()
                     }}>Begrepskatalog</Button>
                 </>
-            ) : ( <Button role="link" size="small" variant="secondary" onClick={(e) => {
-                e.preventDefault();
-                window.open('https://navikt.github.io/spraksjekk/?q=' + higlighetdwords, "_blank");
-            }}>Analyser <ExternalLink style={{paddingTop : "1px"}}/>
+            ) : (<Button role="link" size="small" variant="secondary" onClick={() => {
+                analyzeLink()
+            }}>Analyser <ExternalLink style={{paddingTop: "1px"}}/>
             </Button>)}
         </BubbleMenu>
-    /*https://www.nb.no/ngram/#1_1_2_' + higlighetdwords + '_1_1_0_1800%2C2021_2_2_2_12_2*/
     );
 }
 
