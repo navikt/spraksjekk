d7d72e86d1bee5958ad8166c4e0a12ea03c52be6
EIlif Johansen <eilifjohansen@outlook.com>
2024-01-03 01:13:13 +0100

    La til oppfordring om å kopiere teksten før økten avsluttes

--- src/components/theme/About/About.tsx
+++ src/components/theme/About/About.tsx
@@ -6,7 +6,7 @@ function About() {
         <>
             <p className="a11y-lineheight">Klarspråkshjelpen gir deg umiddelbar tilbakemelding på teksten din.</p>
             <ul className="ListRemoveStyling mt-4">
-                <li><ShakeHands className="inline" title="Håndtrykk"/> Teksten blir ikke lagret.</li>
+                <li><ShakeHands className="inline" title="Håndtrykk"/> Teksten blir ikke lagret — husk å kopiere den før du avslutter.</li>
                 <li><EyeScreened className="inline" title="Personvern"/> Ikke legg inn personopplysninger.</li>
             </ul>
         </>
